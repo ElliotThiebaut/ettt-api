7effd437359b14014c40be6d1b369965b8845fcd
elliot <elliotthiebaut1@gmail.com>
2021-08-06 16:46:56 +0200

    :sparkles: Added basics routes + connexion to mangoDB

--- app.js
+++ app.js
@@ -1,23 +1,52 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const {MongoClient} = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config()
 
 const app = express();
 const port = 3000;
+const mongoUri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/?authSource=admin`
+const client = new  MongoClient(mongoUri)
+let msgColl = client.db('api-messages').collection('messages')
 
-// Fake Database
-let messages = [
-    {
-        "name": "fallen",
-        "message": "Issou crying",
-        'id': 0
-    },
-    {
-        "name": "captain_obvious",
-        "message": "goodbye my friend",
-        'id': 1
+async function mongoConnect() {
+    try {
+        await client.connect();
+
+    } catch (e) {
+        console.error(e);
     }
-];
+}
+
+
+
+
+async function createMessage(newMessage) {
+    await msgColl.insertOne(newMessage)
+}
+
+async function findUserMessages(nameUser) {
+    return await msgColl.find({username: nameUser}).toArray()
+}
+
+async function findAllMessages() {
+    return await msgColl.find().toArray()
+}
+
+async function updateMessage(id, updatedMessage) {
+    const objectId = new ObjectId(id);
+    await msgColl.updateOne({_id : objectId}, {$set: updatedMessage})
+}
+
+async function deleteMessage(id) {
+    const objectId = new ObjectId(id);
+    await msgColl.deleteOne({_id : objectId})
+}
+
+
+
 
 app.use(cors());
 
@@ -25,48 +54,50 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/messages', (req, res) => {
-    if (messages) {
-        res.json(messages)
-    } else {
-        res.send('no messages');
-    }
+
+
+app.get('/messages', async (req, res) => {
+    res.send(await findAllMessages())
 });
 
-app.get('/messages/:id', (req, res) => {
-    // Reading isbn from the URL
-    const id = req.params.id;
 
-    // Searching books for the isbn
-    for (let message of messages) {
+app.get('/messages/:username', async (req, res) => {
 
-        if (message.id === parseInt(id)) {
-            res.json(message);
-            return;
-        }
-    }
-
-    // Sending 404 when not found something is a good practice
-    res.status(404).send('Message not found');
+    res.send(await findUserMessages(req.params.username))
 });
 
-app.post('/new', (req, res) => {
+
+
+
+app.post('/new-message', async (req, res) => {
+
     const newMessage = req.body
-
-    console.log(newMessage)
-    messages.push(newMessage)
+    await createMessage({
+        username: newMessage.name,
+        message: newMessage.message
+    })
 
     res.send('Message received chief')
 });
 
-app.delete('messages/:id', (req, res) => {
-    const id = req.params.id
 
-    messages = messages.filter(i => {
-        return i.id !== parseInt(id);
-    })
+app.post('/update-message/:id', async (req, res) => {
+    await updateMessage(req.params.id, req.body)
+    res.send('Messaged updated chief')
+});
 
-    res.send('Message deleted')
+
+
+
+app.delete('/delete-message/:id', async (req, res) => {
+    await deleteMessage(req.params.id)
+    res.send('Message deleted chief')
 })
 
+
+
+
+
+
+mongoConnect().catch(console.error);
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
