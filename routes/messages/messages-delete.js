68c5461b26c10b343c349ee2e30014b1316b42ae
ElliotThiebaut <elliotthiebaut1@gmail.com>
2021-08-09 23:24:19 +0200

    :goal_net: Added 404 and 500 errors for DELETE routes

--- routes/messages/messages-delete.js
+++ routes/messages/messages-delete.js
@@ -6,6 +6,14 @@ export const router = express.Router()
 //DELETE - delete a existing message
 router.delete('/risichat/delete-message/:id', async (req, res) => {
 
-    await msgColl.deleteOne({message_id : parseInt(req.params.id)})
-    res.send('Message deleted chief')
+    let deletedMessage = await msgColl.deleteOne({message_id : parseInt(req.params.id)})
+
+
+    if (deletedMessage.acknowledged && deletedMessage.deletedCount){
+        res.send({message:'Message deleted'})
+    } else if (!deletedMessage.deletedCount) {
+        res.status(404).send({message:`No message found for message_id ${req.params.id}`})
+    } else {
+        res.status(500).send({message:'A error occurred while updating the message'})
+    }
 })
